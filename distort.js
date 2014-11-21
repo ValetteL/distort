10ab0099f1de512c84cb4e086c31b18c496be573
Isaac Suttell <isaac@isaacsuttell.com>
2014-11-21 15:17:50 -0800

    added matrix quick comments note

--- distort.js
+++ distort.js
@@ -21,6 +21,21 @@
   'use strict';
 
   /**
+   * matrix3d() Quick Notes
+   * https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function
+   *
+   * *
+   *
+   * matrix3d(a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3, a4, b4, c4, d4)
+   *
+   * a1 b1 c1 d1 a2 b2 c2 d2 a3 b3 c3 d3 d4
+   * Are <number> describing the linear transformation.
+   *
+   * a4 b4 c4
+   * Are <length> describing the translation to apply.
+   */
+
+  /**
    * Default Matrix with no distortions
    *
    * @constant
